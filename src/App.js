2f92f2546812ce4604de8ddf0f8a0a839036e4d7
dazede <daisygrace@hotmail.co.nz>
2025-01-29 16:11:49 +1300

    Set up App to get Spotify genres
    Gets Genres from the Spotify categories API. Genres can be selected from the dropdown and display as simple text when selected underneath

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Dropdown from './Dropdown';
+import { Credentials } from './Credentials';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+
+  const spotify = Credentials();
+
+  console.log('RENDERING APP.JS');
+
+  const data = [
+    {value: 1, name: 'A'},
+    {value: 2, name: 'B'},
+    {value: 3, name: 'C'},
+  ];
+
+  const [token, setToken] = useState('');
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    const params = new URLSearchParams();
+    params.append('grant_type', 'client_credentials');
+
+    axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)
+      },
+      data: params,
+      method: 'POST'
+    })
+    .then(tokenResponse => {
+      console.log(tokenResponse.data.access_token);
+      setToken(tokenResponse.data.access_token);
+
+    axios('https://api.spotify.com/v1/browse/categories', {
+      headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token},
+      method: 'GET'
+    })
+    .then (genreResponse => {
+      console.log(genreResponse.data.categories.items);
+      setGenres(genreResponse.data.categories.items);
+    });
+
+    });
+  }, []);
+
+  return(
+    <form onSubmit={() => {}}>
+      <div>
+        <Dropdown options={genres}/>
+      </div>
+    </form>
+    
   );
 }
 
---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '4bd6a01fade9432faf647609c95a3368',
+        ClientSecret: '146aa70e80cc42d0aa681e3c398f026e'
+    }
+}
+
+export { Credentials };
