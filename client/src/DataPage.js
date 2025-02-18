7ef43ae09267b6100bb421e064dabe7cc80537db
dazede <daisygrace@hotmail.co.nz>
2025-02-18 16:59:44 +1300

    responsive pie chart
    set the pie chart up to be responsive depending on screen size

--- client/src/DataPage.js
+++ client/src/DataPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import axios from 'axios';
 import "./App.css";
-import { PieChart, Pie, Cell } from 'recharts';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const DisplayData = () => {
     const [user, setUser] = React.useState("");
     const [userTop, setUserTop] = React.useState("");
+    const [radius, setRadius] = React.useState("");
     
     const GetUserData = async () => {
         try {
@@ -29,7 +30,7 @@ const DisplayData = () => {
                 .sort(([, A], [, B]) => B - A)
                 .slice(0,6)
                 .map(([key, value]) => ({
-                    name: key.includes('alternative') ? key.replace('alternative', 'alt') : key,
+                    name: transformKey(key),
                     value: value
                   }));
             setUserTop(sortedData);
@@ -39,6 +40,21 @@ const DisplayData = () => {
         }
     };
 
+    function transformKey(key){
+        const replacements = {
+            'alternative': 'alt',
+            'psychedelic': 'psyc'
+        }
+
+        for (const [original, replacement] of Object.entries(replacements)) {
+            if (key.includes(original)) {
+                return key.replace(original, replacement);
+            }
+        }
+        
+        return key;
+    }
+
     React.useEffect(() => {
         GetUserData();
         GetUserTopData();
@@ -46,9 +62,11 @@ const DisplayData = () => {
 
     const RADIAN = Math.PI / 180;
     const renderCustomisedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-    const radius = outerRadius + 30;
+    const radius = outerRadius + 20;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    const nameParts = userTop[index].name.split(' ');
+    const shouldSplit = nameParts.length === 2 && (nameParts[0].length > 5 || nameParts[1].length > 5);
 
     return (
         <text 
@@ -59,49 +77,59 @@ const DisplayData = () => {
         textAnchor={x > cx ? "start" : "end"}
         dominantBaseline="central"
       >
-        {userTop[index].name}
+        {shouldSplit ? (
+                nameParts.map((part, i) => (
+                    <tspan x={x} dy={i === 0 ? 0 : '0.8em'} key={i}>{part}</tspan>
+                ))
+            ) : (
+                userTop[index].name
+            )}
       </text>
     );
     };
 
     const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#00CFFF', '#008042'];
 
+    React.useEffect(() => {
+        const updateRadius = () => {
+        setRadius(parseInt(getComputedStyle(document.documentElement).getPropertyValue("--pie-radius"), 10));
+        };
+    
+        updateRadius();
+        window.addEventListener("resize", updateRadius);
+        return () => window.removeEventListener("resize", updateRadius);
+    }, []);
+    
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Aurify</h1>
                 {user && <p>{user}</p>}
-           
-                {userTop && Object.keys(userTop).length > 0 && (
-                //<ResponsiveContainer width="100%" height="300px">
-                    <PieChart width={700} height={700}>
-                        <Pie
-                            data={userTop}
-                            //innerRadius={120}
-                            //outerRadius={180}
-                            //paddingAngle={5}
-                            outerRadius={180}
-                            dataKey="value"
-                            fill="green"
-                            label={renderCustomisedLabel}
-                            cornerRadius={5}
-                            >
-                            {userTop.map((entry, index) => (
-                                <Cell key={`cell-${index}`} 
-                                fill={COLORS[index % COLORS.length]} 
-                                //stroke="none"
-                                strokeWidth={1.5} 
-                               // strokeOpacity={0.4}
-                                
-                             />
-                            ))}
-                        </Pie>
-                    </PieChart>
-                //</ResponsiveContainer>
-                )}
-         
             </header>
-            
+            <div className="pie" >
+                {userTop && Object.keys(userTop).length > 0 && (
+                    <ResponsiveContainer width="100%" height="100%">
+                            <PieChart>
+                                    <Pie
+                                        data={userTop}
+                                        outerRadius={radius}
+                                        dataKey="value"
+                                        label={renderCustomisedLabel}
+                                        cornerRadius={5}
+                                        >
+                                        {userTop.map((entry, index) => (
+                                            <Cell key={`cell-${index}`} 
+                                            fill={COLORS[index % COLORS.length]} 
+                                            //stroke="none"
+                                            //strokeWidth={1.2} 
+                                            //strokeOpacity={0.4}
+                                        />
+                                        ))}
+                                    </Pie>
+                            </PieChart>
+                    </ResponsiveContainer>
+                )}
+            </div>
         </div>
       );
 };
